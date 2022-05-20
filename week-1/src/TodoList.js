e93a4346f44b71a8c6bcb1f8522376b61d5d930b
Squeezedcoma <michael.wayner@bryanuniversity.edu>
2022-05-20 14:41:00 -0400

    todo

--- week-1/src/TodoList.js
+++ week-1/src/TodoList.js
@@ -1,15 +1,12 @@
-import React from 'react';
-import Todo from './Todo.js';
-import './TodoList.css'
-
-
-function TodoList() {
-    return (
-    <div>
-        <div className='Todo1span'></div>
-        <Todo />
-    </div>
-    );
-  }
-  
-  export default TodoList;
+function TodoList(){
+    return(
+        <div>
+        <h1>List of Todos</h1>
+            <div><input type="checkbox"/>Start Stream</div>
+            <div><input type="checkbox"/>Go to Store</div>
+            <div><input type="checkbox"/>Pay Bills</div>
+            <div><input type="checkbox"/>Do Homework</div>
+        </div>
+    )
+    }
+export default TodoList;
--- week-1/src/index.js
+++ week-1/src/index.js
@@ -1,10 +1,12 @@
-import React from "react";
-import  ReactDOM  from "react";
-import TodoList from "./TodoList";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <TodoList />
-    </React.StrictMode>,
-    document.getElementById('root')
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
