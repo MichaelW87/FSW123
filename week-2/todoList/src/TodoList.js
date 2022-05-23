3353a2e0f9f31cfde0b836bbd4c05eb57375949d
Squeezedcoma <michael.wayner@bryanuniversity.edu>
2022-05-23 15:23:02 -0400

    updated

---
+++ week-2/todoList/src/TodoList.js
@@ -0,0 +1,12 @@
+function TodoList(){
+    return(
+        <div>
+        <h1>List of Todos</h1>
+            <div><input type="checkbox"/>Start Stream</div>
+            <div><input type="checkbox"/>Go to Store</div>
+            <div><input type="checkbox"/>Pay Bills</div>
+            <div><input type="checkbox"/>Do Homework</div>
+        </div>
+    )
+    }
+export default TodoList;
