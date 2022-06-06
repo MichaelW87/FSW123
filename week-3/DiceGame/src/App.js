6062fe98f16045471672623eb2a895ac46ab87a9
Squeezedcoma <michael.wayner@bryanuniversity.edu>
2022-06-05 22:18:27 -0400

    week3

---
+++ week-3/DiceGame/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import RollDice from './components/roll/rollDice';
+
+function App() {
+  return (
+    <div className="App">
+      <RollDice />
+    </div>
+  );
+}
+
+export default App;
